27387c538f4f72ad95555f8f062296dde0b2e1da
hyPerdarKness <46135911+hyPerdarKness@users.noreply.github.com>
2020-06-13 23:38:20 +0300

    Update do_sys.php

--- plug/do_sys.php
+++ plug/do_sys.php
@@ -1,4 +1,4 @@
-<?php if(!defined("include")){ echo '<meta http-equiv="refresh" content="0;URL=index.php">'; exit(); }
+<?php if(!defined("include")){ echo '<meta http-equiv="refresh" content="0;URL=../index.php">'; exit(); }
 
 $dbhost = "localhost";
 $dbuser = "root"; //Veritabanı Kullanıcı Adı
